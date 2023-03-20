72a3afdda3c11709ffe531cb4ca12904cb84edbe
Mumin Ali <muminali13@hotmail.co.uk>
2023-03-20 13:18:29 +0000

    test

--- src/components/homescreen/index.js
+++ src/components/homescreen/index.js
@@ -12,6 +12,20 @@ export default class HomeScreen extends Component {
 
 	}
 
+	// a call to fetch weather data via wunderground
+	fetchWeatherData = () => {
+		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
+		// var url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5dd07d3033c59fcd9afe226989a8fdbc";
+		// $.ajax({
+		// 	url: url,
+		// 	dataType: "jsonp",
+		// 	success : this.parseResponse,
+		// 	error : function(req, err){ console.log('API call failed ' + err); }
+		// })
+		// // once the data grabbed, hide the button
+		// this.setState({ display: false });
+	}
+
 	render() {
 		return (
 
