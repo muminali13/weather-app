208c162255263f0e45c51f80354da3c2ef23b2e4
Mumin Ali <muminali13@hotmail.co.uk>
2023-03-23 04:13:30 +0000

    added api call to fetch data

--- src/components/forecast/index.js
+++ src/components/forecast/index.js
@@ -11,33 +11,51 @@ export default class Forecast extends Component {
 	constructor(props) {
 		super(props);
 
+		this.state.location = "London,UK"
 		// const [data, setData] = useState({})
 		// const [location, setLocation] = useState(``)
 
+		this.fetchWeatherData()
 	}
 
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
-		// var url = `https://api.openweathermap.org/data/2.5/weather?q=${this.location},uk&APPID=5dd07d3033c59fcd9afe226989a8fdbc`;
-		// $.ajax({
-		// 	url: url,
-		// 	dataType: "jsonp",
-		// 	success: this.parseResponse,
-		// 	error: function (req, err) { console.log('API call failed ' + err); }
-		// })
-		// // once the data grabbed, hide the button
-		// this.setState({ display: false });
+		var url = `https://api.openweathermap.org/data/2.5/weather?q=${this.location},uk&APPID=5dd07d3033c59fcd9afe226989a8fdbc`;
+		$.ajax({
+			url: url,
+			dataType: "json",
+			success: this.parseResponse,
+			error: function(req, err) { 
+				console.log('API call failed ' + err);
+				window.alert("Couldn't get data for city: " + this.state.location)
+			}
+		})
+		// once the data grabbed, hide the button
+		this.setState({ display: false });
+	}
+
+	parseResponse = (parsed_json) => {
+
+		// set states for fields so they could be rendered later on
+		this.setState({
+			location: parsed_json.name,
+			temperature: parsed_json.main.temp,
+			condition: parsed_json.weather[0].description,
+			feelsLike: parsed_json.main.feels_like,
+			humidiy: parsed_json.main.humidiy,
+			windspeed: parsed_json.wind.speed
+		});
 	}
 
 	render() {
 		return (
 			<div className="app">
-				<NavBar name='Forecast'/>
+				<NavBar name='Forecast' />
 
 				<div className="container">
 
 					<h1>The Next Week:</h1>
-				
+
 					<table>
 						<tr>
 							<td>MON</td>
--- src/components/homescreen/index.js
+++ src/components/homescreen/index.js
@@ -13,35 +13,64 @@ export default class HomeScreen extends Component {
 	constructor(props) {
 		super(props);
 
+		this.onSearch = this.onSearch.bind(this);
+
+		this.state.location = "London"
 		// const [data, setData] = useState({})
 		// const [location, setLocation] = useState(``)
 
+		this.fetchWeatherData()
+		this.fetchForecastData()
 	}
 
 	// a call to fetch weather data via wunderground
-	fetchWeatherData() {
-		if (e.keyCode == 13) {
-			console.log(`Get data for: ${document.getElementById("search-textfield").innerText}`)
+	fetchWeatherData = () => {
+		var url = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.location},uk&units=metric&APPID=5dd07d3033c59fcd9afe226989a8fdbc`;
+		$.ajax({
+			url: url,
+			dataType: "json",
+			success: (parsed_json) => {
+				this.setState({
+					location: parsed_json.name,
+					temperature: parsed_json.main.temp,
+					conditions: parsed_json.weather[0].description,
+					feelsLike: parsed_json.main.feels_like,
+					humidiy: parsed_json.main.humidity,
+					windspeed: parsed_json.wind.speed
+				})},
+			error: function(req, err) { 
+				console.log('API call failed ' + err);
+				window.alert("Couldn't get data for city: " + this.state.location)
+			}
+		})
+	}
 
-			// var url = `https://api.openweathermap.org/data/2.5/weather?q=${this.location},uk&APPID=5dd07d3033c59fcd9afe226989a8fdbc`;
-			// $.ajax({
-			// 	url: url,
-			// 	dataType: "jsonp",
-			// 	success: this.parseResponse,
-			// 	error: function (req, err) { console.log('API call failed ' + err); }
-			// })
-			// once the data grabbed, hide the button
-			// this.setState({ display: false });
+	fetchForecastData = () => {
+		var url = `https://api.openweathermap.org/data/2.5/forecast?q=${this.state.location},uk&cnt=5&APPID=5dd07d3033c59fcd9afe226989a8fdbc`;
+		$.ajax({
+			url: url,
+			dataType: "json",
+			success: (parsed_json) => {
+				this.setState({
+					forecast: parsed_json.list
+				})},
+			error: function(req, err) { 
+				console.log('API call failed ' + err);
+				window.alert("Couldn't get data for city: " + this.state.location)
+			}
+		})
+	}
+
+	onSearch(e) {
+		if (e.keyCode == 13) {
+			let l = document.getElementById("search-textfield").value
+			console.log(`Get data for: ${l}`)
+			this.state.location = l;
+			this.fetchWeatherData()
 		}
 	}
 
 	render() {
-
-		function onSearch(e) {
-			if (e.keyCode == 13) {
-				console.log(`Get data for: ${document.getElementById("search-textfield").value}`)
-			}
-		}
 
 		return (
 			<div className="app">
@@ -49,7 +78,7 @@ export default class HomeScreen extends Component {
 				<div class="header">
 					<div className="search">
 						<FaSearchLocation class="search-icon" />
-						<input type='text' id="search-textfield" onfocus={this.value = ''} placeholder="Search locations..." onKeyUp={onSearch} />
+						<input type='text' id="search-textfield" onfocus={this.value = ''} placeholder="Search locations..." onKeyUp={this.onSearch} />
 					</div>
 				</div>
 
@@ -58,34 +87,31 @@ export default class HomeScreen extends Component {
 					<section className="top">
 						<div className="left">
 							<div className="location">
-								<h2>London</h2>
+								<h2>{this.state.location}</h2>
 							</div>
 
 							<div className="description">
-								<h2>Cloudy</h2>
+								<h2>{this.state.conditions}</h2>
 							</div>
 						</div>
 						<div className="temp">
-							<h1>12°C</h1>
+							<h1>{this.state.temperature}°C</h1>
 						</div>
 					</section>
 					<section className="stats-overview">
-						<h1>
-							Today:
-							<a href='./forecast' class="button" id="view-forecast-button">&#8594;</a>
-						</h1>
+						<h1>Today</h1>
 						<div class="stats-container">
 							<div className="stat-box feels">
 								<h2>Feels Like</h2>
-								<p>9°C</p>
+								<p>{this.state.feelsLike}°C</p>
 							</div>
 							<div className="stat-box humidity">
 								<h2>Humidity</h2>
-								<p>50%</p>
+								<p>{this.state.humidiy}%</p>
 							</div>
 							<div className="stat-box wind">
 								<h2>Windspeed</h2>
-								<p>12 MPH</p>
+								<p>{this.state.windspeed} MPH</p>
 							</div>
 						</div>
 					</section>
@@ -95,17 +121,29 @@ export default class HomeScreen extends Component {
 							<a href='./forecast' class="button" id="view-forecast-button">&#8594;</a>
 						</h1>
 						<div class="week-container">
-							<div className="stat-box">
-								<h2>Feels Like</h2>
-								<p>9°C</p>
+							<div className="weekday-summary-box">
+								<p>FRI</p>
+								<img src="../../../assets/weather-icons/very-cold.png"></img>
 							</div>
-							<div className="stat-box">
-								<h2>Humidity</h2>
-								<p>50%</p>
+							<div className="weekday-summary-box">
+								<p>SAT</p>
+								<img src="../../../assets/weather-icons/icy.png"></img>
 							</div>
-							<div className="stat-box">
-								<h2>Windspeed</h2>
-								<p>12 MPH</p>
+							<div className="weekday-summary-box">
+								<p>SUN</p>
+								<img src="../../../assets/weather-icons/raindrops.png"></img>
+							</div>
+							<div className="weekday-summary-box">
+								<p>MON</p>
+								<img src="../../../assets/weather-icons/wind.png"></img>
+							</div>
+							<div className="weekday-summary-box">
+								<p>TUE</p>
+								<img src="../../../assets/weather-icons/cloudy-sunny.png"></img>
+							</div>
+							<div className="weekday-summary-box">
+								<p>WED</p>
+								<img src="../../../assets/weather-icons/sunny.png"></img>
 							</div>
 						</div>
 					</section>
@@ -115,15 +153,21 @@ export default class HomeScreen extends Component {
 							My Crops
 							<a href='./crops' class="button" id="view-all-crops-button">&#8594;</a>
 						</h2>
-						<div class="crop-button">
-							<img src='https://cdn-icons-png.flaticon.com/128/135/135695.png'></img>
+						<div className="crops-container">
+							<div class="crop-icon">
+								<img src='../../../assets/crop-icons/corn.png'></img>
+							</div>
+							<div class="crop-icon">
+								<img src='../../../assets/crop-icons/wheat.png'></img>
+							</div>
+							<div class="crop-icon">
+								<img src='../../../assets/crop-icons/apple.png'></img>
+							</div>
+							<div class="crop-icon">
+								<img src='../../../assets/crop-icons/cabbage.png'></img>
+							</div>
 						</div>
-						<div class="crop-button">
-							<img src='https://cdn-icons-png.flaticon.com/128/135/135695.png'></img>
-						</div>
-						<div class="crop-button">
-							<img src='https://cdn-icons-png.flaticon.com/128/135/135695.png'></img>
-						</div>
+						
 					</section>
 				</div>
 			</div>
